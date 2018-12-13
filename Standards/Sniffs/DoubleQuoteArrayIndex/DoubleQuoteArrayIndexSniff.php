e9ebc3795fe6de135efd632a877902b5c89cbefc
Umut Işık <umutphp@gmail.com>
2018-12-14 00:33:14 +0300

    Add check for double quote as array index

---
+++ Standards/Sniffs/DoubleQuoteArrayIndex/DoubleQuoteArrayIndexSniff.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This sniff prohibits the comments with word count less than 3
+ *
+ * PHP version 5+
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Your Name <you@domain.net>
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+namespace PHP_CodeSniffer\Standards\CoerciveStandards\Sniffs\DoubleQuoteArrayIndex;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+class CoerciveStandards_Sniffs_DoubleQuoteArrayIndex implements Sniff
+{
+    public $emptyTokens = array(
+        T_DOC_COMMENT_TAG => T_DOC_COMMENT_TAG
+    );
+
+    public $supportedTokenizers = array(
+        'PHP'
+    );
+
+    public function register()
+    {
+        return array(
+            T_DOC_COMMENT_STRING
+        );
+    }
+
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        /*
+         * Ignore inherit doc tag
+         */
+        if ($tokens[$stackPtr]['content'] === "{@inheritdoc}") {
+            return;
+        }
+
+        $sameToken = $phpcsFile->findNext(array(T_DOC_COMMENT_TAG => T_DOC_COMMENT_TAG), ($stackPtr - 2), null, true);
+
+        // If it is a short description
+        if ($tokens[$sameToken]['type'] == 'T_DOC_COMMENT_STAR') {
+            if (str_word_count($tokens[$stackPtr]['content']) < 3) {
+                $error = 'Comment short description is less then 3 words';
+                $phpcsFile->addWarning($error, $stackPtr, 'CommentShortDescriptionWordCount');
+            }
+        }
+    }
+}

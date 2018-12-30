5e67c9a05d1ce476cd98d57bc9a8b9d976dd220e
Umut Işık <umutphp@gmail.com>
2018-12-31 00:08:25 +0300

    Add test folder to version control

---
+++ tests/sample.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Two words
+ */
+function sampleFunction() {
+    $array = array(
+        "first_double_quote" => "bla bla"
+    );
+
+    $array = array("second_double_quote" => "bla bla");
+    $array = array( "third_double_quote" => "bla bla" );
+
+    $array["double_quote"] = "bla bla bla";
+    $array[ "double_quote" ] = "bla bla bla";
+}
